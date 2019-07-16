9c893b4a46be20087a4593926d85818b95c5aa67
dbrazl <danielbrazlopes16@gmail.com>
2019-07-16 05:26:40 -0300

    feature: create routes navegation

---
+++ src/Readme.md
@@ -0,0 +1,7 @@
+# Cloud-repository-front-end
+A app like Google Drive or DropBox to upload and download archives. It's fully doing in NodeJS and ReactJS. I divided the project to Continuous Integration (CI).
+
+## Tech's
+- **React.js** : is a library to create User Interface.
+- **React Router Dom** : is a library to create navegation on front-end.
+- more is comming...
---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import SignIn from '../pages/SingIn';
+import SignUp from '../pages/SingUp';
+import RestoreAccount from '../pages/RestoreAccount';
+import RestoreConfirm from '../pages/RestoreCofirm';
+
+import Profile from '../pages/Profile';
+import Repository from '../pages/Repository';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/register" component={SignUp} />
+      <Route path="/restore" component={RestoreAccount} />
+      <Route path="/restore/confirm" component={RestoreConfirm} />
+
+      <Route path="/profile" component={Profile} />
+      <Route path="/repository" component={Repository} />
+    </Switch>
+  );
+}
